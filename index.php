62737d1127fccb5ecdcbb89bdd59690168ad646c
Nadim Dahdouli <nadim.dahdouli@gmail.com>
2015-09-02 18:32:20 +0200

    index.php cleanup and removed Indexer.php

--- index.php
+++ index.php
@@ -16,17 +16,6 @@ Config::write("folder.movies", "D:\Videos\Movies");
 
 echo "<pre>";
 
-/*
-  foreach (glob("TV/*") as $show) {
-  $showTitle = preg_replace("/^(.+\/)/i", "", $show);
-  foreach(glob("$show/*") as $season) {
-  print_r($season."\n");
-  }
-  }
- */
-
-
-
 $mIndexer = new MovieIndexer(Config::read("folder.movies"));
 print_r($mIndexer->indexUnindexed());
 
@@ -34,42 +23,11 @@ foreach ($mIndexer->getIndexed() as $movie) {
     echo "$movie->Title\n<img src='$movie->Poster'></img>\n\n";
 }
 
-//print_r($mIndexer->getIndexed());
-
-/*
-  $movieTitles = $mIndexer->getMovieTitles();
-  foreach ($movieTitles as $movieTitle) {
-  echo $movieTitle."\n";
-  print_r($mIndexer->getMovieMetadata($movieTitle));
-  echo "\n\n";
-  }
-  print_r(NULL);
- */
-
-/*
-  foreach ($tv as $show) {
-  foreach ($show as $season) {
-  foreach ($season as $episode) {
-  $matches = array();
-  if (getTvMetadata($episode, $matches)) {
-  $title = urlencode(trim($matches[0]));
-  $json = file_get_contents("http://omdbapi.com/?t=$title&Season=$matches[1]&Episode=$matches[2]");
-  echo "$matches[0] $matches[1] $matches[2]\n";
-  $details = json_decode($json);
-
-  if ($details->Response == "True") {
-  echo $details->Title . "\n\n";
-  }
-  }
-  }
-  }
-  }
- */
-
-function getTvMetadata($episode, &$matches) {
-    if (preg_match("'^(.+)S([0-9]+)E([0-9]+).*$'i", $episode, $matches)) {
-        array_shift($matches);
-        return $matches;
-    }
-    return FALSE;
-}
+//
+//function getTvMetadata($episode, &$matches) {
+//    if (preg_match("'^(.+)S([0-9]+)E([0-9]+).*$'i", $episode, $matches)) {
+//        array_shift($matches);
+//        return $matches;
+//    }
+//    return FALSE;
+//}
